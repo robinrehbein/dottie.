b4d0586778047fb954768978976446d9ddb6c111
Robin Rehbein <robdeerleg@gmail.com>
2025-10-31 09:30:05 +0100

    inital

---
+++ app/src/main/java/de/robinrehbein/punkt/MainActivity.kt
@@ -0,0 +1,28 @@
+package de.robinrehbein.punkt
+
+import android.os.Bundle
+import android.view.ViewTreeObserver
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import de.robinrehbein.punkt.ui.theme.PunktTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val gameView = GameView(this)
+        setContentView(gameView)
+
+
+        // enableEdgeToEdge()
+
+    }
+}
